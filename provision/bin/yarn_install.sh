29020b40ba9d080e048a186b676577e4843ad704
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2018-08-09 13:46:17 +0100

    node pkgs

---
+++ provision/bin/yarn_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+PROVISION_DIR=/tmp/provision
+source ${PROVISION_DIR}/bin/set_proxy.sh
+
+installPkgs() {
+  if curl -L github.com | grep "Web Page Blocked"; then
+    echo "We are behind the Corporate Proxy, Setting http_proxy"
+    yarn config set proxy http://${ONPREMISE_HTTP_PROXY}
+    yarn config set https-proxy http://${ONPREMISE_HTTPS_PROXY}
+  fi
+
+  yarn add \
+    sshp \
+    json
+}
+
+main() {
+    installPkgs
+}
+main
